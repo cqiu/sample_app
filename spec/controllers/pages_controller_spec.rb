78209a868e50d35336b30631bc4dc99c712c211b
Your Name <chunyu.qiu@sage.com>
2011-04-13 11:42:24 -0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  describe "GET 'home'" do
+   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home' 
       response.should have_selector("title", 
-                        :content => " | Home")
+                        :content =>  " | Home")
     end
   end
 
