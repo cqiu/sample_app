0f54bc5f1bcef64efd095ede46e063e26fe4f7fb
Your Name <chunyu.qiu@sage.com>
2011-04-14 14:20:58 -0400

    Added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,10 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def current_user=(user)
+      @current_user = user
+  end
+
   def current_user
     @current_user ||= user_from_remember_token
   end
@@ -20,6 +24,10 @@ module SessionsHelper
 
   def current_user?(user)
     user == current_user
+  end
+
+  def authenticate
+    deny_access unless signed_in?
   end
 
   def deny_access
